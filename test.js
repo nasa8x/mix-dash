157ccfd765a02074186a940360fdff336a508e27
Nasa Nguyen <nasa8x@gmail.com>
2018-08-14 09:53:41 +0700

    init

---
+++ test.js
@@ -0,0 +1,35 @@
+var _ = require('./src/main');
+
+// var moment = require('moment');
+
+// var start = moment().toDate();
+
+
+// var end = moment().add(1,'hours').toDate();
+
+// var ran = _.randomDate(start, end);
+// console.log(start);
+// console.log(end);
+// console.log(ran);
+
+// console.log(_.numberFormat(397668220.07591987));
+
+
+// console.log(0.5690809809.toFixed(4));
+
+// console.log(_.toKb(10.5690809809, 4));
+
+
+var map = {
+    name : "id",
+    amount : "total",
+    reported : "updated",
+    // date : "issued"
+};
+var a = {
+    name : "Foo",
+    amount: 55,
+    reported : false,
+    date : "10/01/2001"
+};
+console.log(_.mirror(a, map));
